b898b713426d3d1ece13f60f6cc611d320b48816
Steve Kane <kanesteven@gmail.com>
2020-08-28 15:17:47 -0500

    added proper nav test w/ baked obstacles

--- sampleproject/Assets/Samples/Steve/Scripts/Systems/PathFindingTestSystem.cs
+++ sampleproject/Assets/Samples/Steve/Scripts/Systems/PathFindingTestSystem.cs
@@ -1,17 +1,115 @@
-﻿using Unity.Burst;
-using Unity.Jobs;
+﻿using Unity.Jobs;
 using Unity.Collections;
-using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.Transforms;
 using Unity.NetCode;
 using UnityEngine;
 using UnityEngine.Experimental.AI;
-using static Unity.Mathematics.math;
+
+public struct PolygonSearchConfig {
+  public NavMeshLocation start;
+  public NavMeshLocation end;
+  public int maxNodesTraversedPerUpdate;
+}
+
+public struct StraightPathConfig {
+  public NavMeshLocation start;
+  public NavMeshLocation end;
+  public NativeArray<PolygonId> polygonIds;
+  public int polyCount;
+  public int maxPathLength;
+}
+
+public struct PathConfig {
+  public Allocator allocator;
+  public NavMeshWorld navMeshWorld;
+  public Vector3 start;
+  public Vector3 end;
+  public Vector3 searchExtents;
+  public int maxPathLength;
+  public int maxStackSize;
+  public int maxNodesTraversedPerUpdate;
+  public int agentTypeId;
+}
 
 [UpdateInGroup(typeof(ClientSimulationSystemGroup))]
 public class PathFindingTestSystem : SystemBase {
+  // TODO: This has no error-handling but it may be fairly common that a path cannot be found
+  //       or that some inputs to the query such as start and end locations are not sufficiently-close
+  //       to the navmesh. Something should happen... not sure what... 
+  public int ComputeConnectedPolygons([ReadOnly] PolygonSearchConfig searchConfig, NavMeshQuery navMeshQuery, NativeArray<PolygonId> polygonIds) {
+    PathQueryStatus beginPathStatus = navMeshQuery.BeginFindPath(searchConfig.start, searchConfig.end);
+    PathQueryStatus updateStatus;
+    do {
+      updateStatus = navMeshQuery.UpdateFindPath(searchConfig.maxNodesTraversedPerUpdate, out int iterationsPerformed);
+    } while (updateStatus == PathQueryStatus.InProgress);
+    PathQueryStatus endPathStatus = navMeshQuery.EndFindPath(out int pathLength);
+
+    return navMeshQuery.GetPathResult(polygonIds);
+  }
+
+  // This code is copied from a goddamn forum and some stupid library made by Unity found here
+  // https://forum.unity.com/threads/how-to-use-navmeshquery-get-path-points.646861/
+  // https://github.com/Unity-Technologies/UniteAustinTechnicalPresentation/blob/master/StressTesting/Assets/Scripts/Utils/PathUtils.cs
+  // I am not proud of this and will someday amend my wickedness but for now...it's 82 degrees in this office and 11pm
+  public int ComputeStraightPath([ReadOnly] StraightPathConfig pathConfig, NavMeshQuery navMeshQuery, NativeArray<NavMeshLocation> straightPath) {
+    if (pathConfig.start.polygon == pathConfig.end.polygon) {
+      straightPath[0] = pathConfig.start;
+      straightPath[1] = pathConfig.end;
+      return 2;
+    } else {
+      int straightPathCount = 0;
+      NativeArray<StraightPathFlags> straightPathFlags = new NativeArray<StraightPathFlags>(pathConfig.polyCount, Allocator.Temp);
+      NativeArray<float> vertexSide = new NativeArray<float>(pathConfig.polyCount, Allocator.Temp);
+      PathQueryStatus straightPathStatus = PathUtils.FindStraightPath(
+        navMeshQuery, 
+        pathConfig.start.position, 
+        pathConfig.end.position, 
+        pathConfig.polygonIds, 
+        pathConfig.polyCount, 
+        ref straightPath, 
+        ref straightPathFlags, 
+        ref vertexSide, 
+        ref straightPathCount,
+        pathConfig.maxPathLength);
+      
+      straightPathFlags.Dispose();
+      vertexSide.Dispose();
+      return straightPathCount;
+    }
+  }
+
+  public int TryComputePath([ReadOnly] PathConfig pathConfig, NativeArray<NavMeshLocation> straightPath) {
+    var navMeshQuery = new NavMeshQuery(pathConfig.navMeshWorld, Allocator.TempJob, pathConfig.maxStackSize);
+    var polygonIds = new NativeArray<PolygonId>(pathConfig.maxPathLength, Allocator.TempJob);
+    PolygonSearchConfig searchConfig = new PolygonSearchConfig {
+      start = navMeshQuery.MapLocation(pathConfig.start, pathConfig.searchExtents, pathConfig.agentTypeId),
+      end = navMeshQuery.MapLocation(pathConfig.end, pathConfig.searchExtents, pathConfig.agentTypeId),
+      maxNodesTraversedPerUpdate = 32,
+    };
+    int polyCount = ComputeConnectedPolygons(searchConfig, navMeshQuery, polygonIds);
+
+    StraightPathConfig straightPathConfig = new StraightPathConfig {
+      start = searchConfig.start,
+      end = searchConfig.end,
+      polyCount = polyCount,
+      maxPathLength = pathConfig.maxPathLength,
+      polygonIds = polygonIds
+    };
+
+    int pathLength = ComputeStraightPath(straightPathConfig, navMeshQuery, straightPath);
+
+    navMeshQuery.Dispose();
+    polygonIds.Dispose();
+    return pathLength;
+  }
+
+  public void DrawDebugPath(NativeArray<NavMeshLocation> path, int length) {
+    for (int i = 1; i < length; i++) {
+      Debug.DrawLine(path[i-1].position, path[i].position, Color.red);
+    }
+  }
+
   protected override void OnUpdate() {
     NavMeshWorld navMeshWorld = NavMeshWorld.GetDefaultWorld();
 
@@ -22,62 +120,22 @@ public class PathFindingTestSystem : SystemBase {
     Entities
     .WithoutBurst()
     .ForEach((ref Translation translation, in Traveler traveler) => {
-      const int MAX_PATH_LENGTH = 128;
-      const int MAX_NODES_TRAVERSED_PER_UPDATE = 32;
-      Vector3 SEARCH_EXTENTS = new Vector3(2, 2, 2);
+      PathConfig pathConfig = new PathConfig {
+        allocator = Allocator.Temp,
+        navMeshWorld = navMeshWorld,
+        start = translation.Value,
+        end = traveler.targetPosition,
+        searchExtents = new Vector3(2,2,2),
+        maxPathLength = 128,
+        maxStackSize = 64,
+        maxNodesTraversedPerUpdate = 32,
+        agentTypeId = 0
+      };
+      NativeArray<NavMeshLocation> path = new NativeArray<NavMeshLocation>(pathConfig.maxPathLength, Allocator.Temp);
+      int pathLength = TryComputePath(pathConfig, path);
 
-      // TODO: This has no error-handling but it may be fairly common that a path cannot be found
-      //       or that some inputs to the query such as start and end locations are not sufficiently-close
-      //       to the navmesh. Something should happen... not sure what... 
-
-      using (var polygonIds = new NativeArray<PolygonId>(MAX_PATH_LENGTH, Allocator.Temp))
-      using (var navMeshQuery = new NavMeshQuery(navMeshWorld, Allocator.Temp, MAX_PATH_LENGTH))
-      {
-        Vector3 worldStart = (Vector3)translation.Value;
-        Vector3 worldEnd = (Vector3)traveler.targetPosition;
-        NavMeshLocation navMeshStart = navMeshQuery.MapLocation(worldStart, SEARCH_EXTENTS, 0);
-        NavMeshLocation navMeshEnd = navMeshQuery.MapLocation(worldEnd, SEARCH_EXTENTS, 0);
-        PathQueryStatus beginPathStatus = navMeshQuery.BeginFindPath(navMeshStart, navMeshEnd);
-        PathQueryStatus updateStatus;
-        do {
-          updateStatus = navMeshQuery.UpdateFindPath(MAX_NODES_TRAVERSED_PER_UPDATE, out int iterationsPerformed);
-        } while (updateStatus == PathQueryStatus.InProgress);
-        PathQueryStatus endPathStatus = navMeshQuery.EndFindPath(out int pathLength);
-        int length = navMeshQuery.GetPathResult(polygonIds);
-
-        // TODO: This appears to work but needs much more rigorous testing with non-trivial navmeshes, obstacles, etc
-        // TODO: This MAY not be working when the start location is in the same polygon as the end location. 
-        //       I think this is relatively easy to handle though as you just draw a path straight from start to end..
-        //       This should probably be an early-out optimization.... 
-
-        // This code is copied from a goddamn forum and some stupid library made by Unity found here
-        // https://forum.unity.com/threads/how-to-use-navmeshquery-get-path-points.646861/
-        // https://github.com/Unity-Technologies/UniteAustinTechnicalPresentation/blob/master/StressTesting/Assets/Scripts/Utils/PathUtils.cs
-        // I am not proud of this and will someday amend my wickedness but for now...it's 82 degrees in this office and 11pm
-        NativeArray<NavMeshLocation> straightPath = new NativeArray<NavMeshLocation>(pathLength, Allocator.Temp);
-        NativeArray<StraightPathFlags> straightPathFlags = new NativeArray<StraightPathFlags>(pathLength, Allocator.Temp);
-        NativeArray<float> vertexSide = new NativeArray<float>(pathLength, Allocator.Temp);
-        int straightPathCount = 0;
-        PathQueryStatus straightPathStatus = PathUtils.FindStraightPath(
-          navMeshQuery, 
-          worldStart, 
-          worldEnd, 
-          polygonIds, 
-          pathLength, 
-          ref straightPath, 
-          ref straightPathFlags, 
-          ref vertexSide, 
-          ref straightPathCount,
-          MAX_PATH_LENGTH);
-        string path = "";
-        for (int i = 0; i < straightPathCount; i++) {
-          path += straightPath[i].position.ToString();
-        }
-        for (int i = 1; i < straightPathCount; i++) {
-          Debug.DrawLine(straightPath[i-1].position, straightPath[i].position, Color.red);
-        }
-        Debug.Log(path);
-      }
+      DrawDebugPath(path, pathLength);
+      path.Dispose();
     }).Run();
   }
 }
